18118bd2d6c9bf21cbd65aa5d10625951a09d2e7
HaselmutGames <haselmut.games@gmail.com>
2025-03-30 12:49:10 +0200

    Add and experiment with colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<script>
-    let name = "Hammoud";
+<script lang="ts">
+    let headline = "HaselmutGames.com";
+    let question = "Who am I?";
     let bio = "I'm Hammoud, a Software Developer aspiring to become a Game Developer.🧑‍💻🎮"
     let socials = [
         { name: "GitHub", url: "https://github.com/HaselmutGames" },
-        {name: "Twitch", url: "https://www.twitch.tv/haselmutgames"},
+        { name: "Twitch", url: "https://www.twitch.tv/haselmutgames" },
         { name: "YouTube", url: "https://www.youtube.com/@HaselmutGames" },
         { name: "X", url: "https://x.com/HaselmutGames" },
     ];
 </script>
 
 <main class="container">
-    <h1>{name}</h1>
+    <div class="headline">
+        <h1>{headline}</h1>
+    </div>
+    <h1>{question}</h1>
     <p>{bio}</p>
 
     <ul>
@@ -28,9 +29,20 @@
 </main>
 
 <style>
+    :global(html){
+        font-family: 'Courier New', Courier, monospace;
+        background-color: black;
+        color: white;
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
     .container{
         text-align: center;
-        margin-top: 50px;
+        margin-top: 50px;  
+    }
+    .headline{
+        color: orange;
     }
     ul{
         list-style: none;
